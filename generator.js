fba9443a07bd5ae107b352a1932333f2f71c30fb
floss-order <flossOrder@gmail.com>
2020-08-19 22:12:10 +0300

    Fixed spacing

--- generator.js
+++ generator.js
@@ -3,34 +3,34 @@ const path = require('path')
 
 
 function generatePackage(name, description, keywords=[], author, license='ISC', dependencies, devDependencies) {
-    let package = {
-        name,
-        version: "1.0.0",
-        description,
-        "main": "server.js",
-        "scripts": {
-          "test": "echo \"Error: no test specified\" && exit 1",
-          "start": "NODE_ENV=production node server.js",
-          "dev": "NODE_ENV=development nodemon server.js",
-        },
-        keywords,
-        author,
-        license,
-        "dependencies": {
-          "express": "^4.17.1",
-          ...dependencies
-        },
-        "devDependencies": {
-          "nodemon": "^2.0.4",
-          ...devDependencies
-        }
-          
+  let package = {
+    name,
+    version: "1.0.0",
+    description,
+    "main": "server.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1",
+      "start": "NODE_ENV=production node server.js",
+      "dev": "NODE_ENV=development nodemon server.js",
+    },
+    keywords,
+    author,
+    license,
+    "dependencies": {
+      "express": "^4.17.1",
+      ...dependencies
+    },
+    "devDependencies": {
+      "nodemon": "^2.0.4",
+      ...devDependencies
     }
+        
+  }
 
-    fs.writeFileSync('package.json', JSON.stringify(package, null, 2), 'utf-8')
+  fs.writeFileSync('package.json', JSON.stringify(package, null, 2), 'utf-8')
 }
 
 function concat(filename, ...files) {
-    const content = files.map(file => fs.readFileSync(file)).join('\n')
-    fs.writeFileSync(path.join(__dirname, filename), content, 'utf-8')
+  const content = files.map(file => fs.readFileSync(file)).join('\n')
+  fs.writeFileSync(path.join(__dirname, filename), content, 'utf-8')
 }
